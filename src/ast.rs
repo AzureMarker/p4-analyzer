6af87c64ddebbebf2343bec914734f96186e4fb5
Mark Drobnak <mtd8050@rit.edu>
2020-11-29 18:27:12 -0500

    Parse control blocks with boolean variable assignments

--- src/ast.rs
+++ src/ast.rs
@@ -1,9 +1,48 @@
 #[derive(Debug)]
 pub struct Program {
-    pub statements: Vec<Statement>,
+    pub declarations: Vec<Declaration>,
+}
+
+#[derive(Debug)]
+pub enum Declaration {
+    Control(ControlDecl),
+}
+
+#[derive(Debug)]
+pub struct ControlDecl {
+    pub name: String,
+    pub params: Vec<Param>,
+    pub body: Vec<Statement>,
+}
+
+#[derive(Debug)]
+pub struct Param {
+    pub direction: ParamDirection,
+    pub ty: String,
+    pub name: String,
+}
+
+#[derive(Debug)]
+pub enum ParamDirection {
+    In,
+    Out,
+    InOut,
 }
 
 #[derive(Debug)]
 pub enum Statement {
-    Import { path: String },
+    Assignment(Assignment),
+    // actions, tables, apply block
+}
+
+#[derive(Debug)]
+pub struct Assignment {
+    pub ty: String,
+    pub name: String,
+    pub value: Expr,
+}
+
+#[derive(Debug)]
+pub enum Expr {
+    Bool(bool),
 }
